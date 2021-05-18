0777c9d0b005d895aef71d6eda61ad2b6bcdc516
madhua <madhua@synebytes.com>
2021-05-18 19:26:34 +0530

    Initial commit

---
+++ src/main/java/com/sb/grpcfilix/GrpcFilixApplication.java
@@ -0,0 +1,13 @@
+package com.sb.grpcfilix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcMovixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcMovixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/grpcfilix/GrpcFilixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.grpcfilix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcFilixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
